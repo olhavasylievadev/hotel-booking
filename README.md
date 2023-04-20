37184a0b5ae0f919f39b56762d871d3bf149032b
olhavasylievadev <106344802+olhavasylievadev@users.noreply.github.com>
2023-04-20 19:20:09 +0200

    Adding emails

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ This is the repository for my project that imitates reserving hotel rooms.
 - Uses the [chi router](https://github.com/go-chi/chi)
 - Uses Alex Edwards [SCS](https://github.com/alexedwards/scs/v2) session management
 - Uses [NoSurf](https://github.com/justinas/nosurf)
+- Uses xhit [go-simple-mail](https://github.com/xhit/go-simple-mail)
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/olhavasylievadev/hotel-booking/internal/config"
 	"github.com/olhavasylievadev/hotel-booking/internal/driver"
@@ -153,6 +154,38 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
+
+	// send notifications, first to guest
+	htmlMessage := fmt.Sprintf(`
+		<strong>Resesrvation Confirmation</strong><br/>
+		Dear %s, <br/>
+		This is to confirm your reservation from %s to %s.
+`, reservation.FirstName, reservation.StartDate.Format("2006-01-02"), reservation.EndDate.Format("2006-01-02"))
+
+	msg := models.MailData{
+		To:       reservation.Email,
+		From:     "welcome@hotel.com",
+		Subject:  "Reservation Confirmation",
+		Content:  htmlMessage,
+		Template: "basic.html",
+	}
+
+	m.App.MailChan <- msg
+
+	// send notification to a property owner
+	htmlMessage = fmt.Sprintf(`
+		<strong>Resesrvation Notification</strong><br/>
+		A reservation has been made for %s, from %s to %s.
+`, reservation.Room.RoomName, reservation.StartDate.Format("2006-01-02"), reservation.EndDate.Format("2006-01-02"))
+
+	msg = models.MailData{
+		To:      "you@there.com",
+		From:    "welcome@hotel.com",
+		Subject: "Reservation Notification",
+		Content: htmlMessage,
+	}
+
+	m.App.MailChan <- msg
 
 	m.App.Session.Put(r.Context(), "reservation", reservation)
 
