48aa727073b3c89000f014c8b84e18be918f7eb7
olhavasylievadev <106344802+olhavasylievadev@users.noreply.github.com>
2023-04-27 18:25:42 +0200

    Fixed run function

---
+++ migrations/20230427145448_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "public"."users"("first_name","last_name","email","password","access_level","created_at","updated_at")
+VALUES
+    (E'Olha',E'Vasylieva',E'admin@admin.com',E'$2a$12$O9JzBUP53dczvyXDh8GYiuzp6eG9yD2iDrla38Nf/oGPmQzyj.dxK',3,E'2023-01-01 00:00:00',E'2023-01-01 00:00:00');
